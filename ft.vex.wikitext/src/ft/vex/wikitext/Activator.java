7bd3b34b86633aaf35084a7578ceb13fff2d09c2
Florian Thienel <florian@thienel.org>
2011-10-23 11:51:11 +0200

    added file headers
    Signed-off-by: Florian Thienel <florian@thienel.org>

--- ft.vex.wikitext/src/ft/vex/wikitext/Activator.java
+++ ft.vex.wikitext/src/ft/vex/wikitext/Activator.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Florian Thienel and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * 		Florian Thienel - initial API and implementation
+ *******************************************************************************/
 package ft.vex.wikitext;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
