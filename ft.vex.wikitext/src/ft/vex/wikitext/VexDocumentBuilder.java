422146c1614edaed6dd02fa42d745ce6a1cd2b39
Florian Thienel <florian@thienel.org>
2012-01-04 10:31:24 +0100

    reapplied underscores in element names since Vex now uses Batik CSS
    Signed-off-by: Florian Thienel <florian@thienel.org>

--- ft.vex.wikitext/src/ft/vex/wikitext/VexDocumentBuilder.java
+++ ft.vex.wikitext/src/ft/vex/wikitext/VexDocumentBuilder.java
@@ -42,13 +42,13 @@ public class VexDocumentBuilder extends DocumentBuilder {
 	public static final Map<BlockType, QualifiedName> BLOCK_ELEMENTS = new HashMap<BlockType, QualifiedName>();
 	static {
 		for (BlockType blockType : BlockType.values())
-			BLOCK_ELEMENTS.put(blockType, new QualifiedName(WIKITEXT_NAMESPACE, blockType.name().replaceAll("_", "")));
+			BLOCK_ELEMENTS.put(blockType, new QualifiedName(WIKITEXT_NAMESPACE, blockType.name()));
 	}
 
 	public static final Map<SpanType, QualifiedName> SPAN_ELEMENTS = new HashMap<SpanType, QualifiedName>();
 	static {
 		for (SpanType spanType : SpanType.values())
-			SPAN_ELEMENTS.put(spanType, new QualifiedName(WIKITEXT_NAMESPACE, spanType.name().replaceAll("_", "")));
+			SPAN_ELEMENTS.put(spanType, new QualifiedName(WIKITEXT_NAMESPACE, spanType.name()));
 	}
 
 	private Document document;
